1690cb4b2920d685edf53e5d7fc971545680f72d
Si9num <yastremskykirill@gmail.com>
2023-03-10 01:18:12 +0300

    feat: add block

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import img from './assets/weather/cloud.png'
 import './App.css';
-
+import { weatherArr } from './weather';
+import { store } from "./store/store.js";
+import { getWeather } from './weather';
+import { useEffect } from 'react';
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className="app">
+      <div className='mainWin'>
+        <div className="text">
+    <p>{JSON.parse(sessionStorage.getItem('weather'))[0].date}</p>
+    <p>{Math.round(JSON.parse(sessionStorage.getItem('weather'))[0].temp)}</p>
+    <p>REAL FEEL {Math.round(JSON.parse(sessionStorage.getItem('weather'))[0].feels)}</p>
+    <p>HUMIDITY {JSON.parse(sessionStorage.getItem('weather'))[0].humid}</p>
+    </div>
+    <div className='weatherIcon'>
+      <img src={require(`./assets/weather/${JSON.parse(sessionStorage.getItem('weather'))[0].weather}.png`)}></img>
+    </div>
+      </div>
+      <div className='weekdays'>  
+
+      </div>
     </div>
   );
 }
---
+++ src/weather.js
@@ -0,0 +1,30 @@
+
+export let weatherArr = [];
+
+export async function getWeather(){
+    let res = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=Minsk&appid=${process.env.REACT_APP_KEY}&units=metric`);
+    let weather = await res.json()
+
+    for(let i = 0; i < weather.list.length; i++){
+        weatherArr.push({
+            date: weather.list[i].dt_txt.slice(0,10).split('-').reverse().join('.'),
+            temp: weather.list[i].main.temp,
+            feels: weather.list[i].main.feels_like,
+            humid: weather.list[i].main.humidity,
+            weather:weather.list[i].weather[0].main
+        })
+    }
+    let tmpArray = [];
+
+function itemCheck(item) {
+    if (tmpArray.indexOf(item.date) === -1) {
+        tmpArray.push(item.date);
+        return true
+    }
+    return false;
+}
+
+weatherArr= weatherArr.filter((item) => itemCheck(item))
+      console.log(weatherArr);
+    sessionStorage.setItem('weather',JSON.stringify(weatherArr))
+}
